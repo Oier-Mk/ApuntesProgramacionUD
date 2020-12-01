5be0b37d5107481017ac29549f1f91946d8c8a73
Oier-Mk <oiermentxaka@opendeusto.es>
2020-12-01 16:03:43 +0100

    20201201

--- Apuntes/src/Library/Counter.java
+++ Apuntes/src/Library/Counter.java
@@ -2,13 +2,22 @@ package Library;
 
 public class Counter {
 	private int c = 0;
-	public Counter() {	}
+	public Counter() {}
 	
 	public int getCounter() {
 		return c;
 	}
 	public void setCounter(int c) {
 		this.c = c;
+	}
+
+	public void inc() {
+		c++;
+	}
+
+	@Override
+	public String toString() {
+		return c+"";
 	}
 	
 }
--- Apuntes/src/Prueba/Prueba.java
+++ Apuntes/src/Prueba/Prueba.java
@@ -1,17 +1,11 @@
 package Prueba;
 
+import Library.Counter;
+
 public class Prueba{
 	public static void main(String[] args) {
-		System.out.println(f(5));
-		System.out.println(combinatoria(90,5));
+		Counter c = new Counter();
+		c.inc();
+		System.out.println(c);
 	}	 
-	static int combinatoria(int a, int b){
-		return (f(a)/(f(b)*f(a-b)));
-	}
-	static int f(int n) {
-		if(n==0)return 1;
-		else {
-			return n*f(n-1);
-		}
-	}
 }
